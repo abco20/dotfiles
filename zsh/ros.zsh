fa0491c672e9ba2f9e87896c53191ce9f3e45efc
abco20 <meijizifuyong@gmail.com>
2025-03-10 12:58:25 +0000

    wip

--- zsh/ros.zsh
+++ zsh/ros.zsh
@@ -1,10 +1,13 @@
-DISTRO="humble"
+DISTRO="jazzy"
 
 if [ $DISTRO = "humble" ]; then
 source /opt/ros/humble/setup.zsh
 source /usr/share/colcon_cd/function/colcon_cd.sh
+elif [ $DISTRO = "jazzy" ]; then
+source /opt/ros/jazzy/setup.zsh
+source /usr/share/colcon_cd/function/colcon_cd.sh
 fi
 
-eval "$(register-python-argcomplete3 ros2)"
-eval "$(register-python-argcomplete3 colcon)"
+eval "$(register-python-argcomplete ros2)"
+eval "$(register-python-argcomplete colcon)"
 export RCUTILS_COLORIZED_OUTPUT=1
