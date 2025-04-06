dbbae4b811d918a100d7815ff8901dfe2f162b82
abco20 <meijizifuyong@gmail.com>
2025-04-06 04:12:18 +0000

    addlazygit

--- install.sh
+++ install.sh
@@ -13,6 +13,14 @@ wget https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18
 sudo dpkg -i git-delta_0.18.2_amd64.deb
 rm git-delta_0.18.2_amd64.deb
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit.tar.gz
+rm lazygit
+
+
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 echo "Create dotfile links."
